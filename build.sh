39bfb1dce2efd45458fe1653b653ffbb41bf422f
Mauricio-100 <ceoseshell@gmail.com>
2026-03-18 23:42:13 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-gcc src/module.c src/fe.c -DFE_STANDALONE -O3 -o fe -Wall -Wextra -std=c99 -pedantic
+# On compile vers le binaire 'gd'
+mv src/fe.c src/gsc.c
+mv src/fe.h src/gsc.c
+gcc src/module.c src/gsc.c -DFE_STANDALONE -O3 -o gd -Wall -Wextra -std=c99 -pedantic

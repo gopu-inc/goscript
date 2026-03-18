b4068160b9300c4e8c980eef2499e28a25922c72
Mauricio-100 <ceoseshell@gmail.com>
2026-03-18 22:38:19 +0100

    Update fe.c

--- src/fe.c
+++ src/fe.c
@@ -1,6 +1,6 @@
 /*
-** Copyright (c) 2020 rxi
-**
+** Copyright (c) 2026 GOPU INC
+** THE FREE SOURCE LANGUAGE DEV GOSCRIPT GJS
 ** Permission is hereby granted, free of charge, to any person obtaining a copy
 ** of this software and associated documentation files (the "Software"), to
 ** deal in the Software without restriction, including without limitation the

28871609dbeea1674f4d680cd60477eac73cad8b
Mauricio-100 <ceoseshell@gmail.com>
2026-03-18 17:44:52 +0100

    Refactor for loop and add extra node handling

--- src/ast.c
+++ src/ast.c
@@ -13,6 +13,7 @@ Node* create_program(Node* first_stmt) {
     node->type = NODE_PROGRAM;
     node->left = first_stmt;
     node->right = NULL;
+    node->extra = NULL;
     return node;
 }
 
@@ -21,6 +22,7 @@ Node* create_block(Node* statements) {
     node->type = NODE_BLOCK;
     node->left = statements;
     node->right = NULL;
+    node->extra = NULL;
     return node;
 }
 
@@ -28,6 +30,7 @@ Node* create_number(double value) {
     Node* node = (Node*)allocate(sizeof(Node));
     node->type = NODE_LITERAL_NUMBER;
     node->data.number_value = value;
+    node->left = node->right = node->extra = NULL;
     return node;
 }
 
@@ -35,6 +38,7 @@ Node* create_string(const char* value) {
     Node* node = (Node*)allocate(sizeof(Node));
     node->type = NODE_LITERAL_STRING;
     node->data.string_value = copy_string(value);
+    node->left = node->right = node->extra = NULL;
     return node;
 }
 
@@ -42,6 +46,7 @@ Node* create_bool(bool value) {
     Node* node = (Node*)allocate(sizeof(Node));
     node->type = NODE_LITERAL_BOOL;
     node->data.bool_value = value;
+    node->left = node->right = node->extra = NULL;
     return node;
 }
 
@@ -49,6 +54,7 @@ Node* create_variable(const char* name) {
     Node* node = (Node*)allocate(sizeof(Node));
     node->type = NODE_VARIABLE;
     node->data.identifier = copy_string(name);
+    node->left = node->right = node->extra = NULL;
     return node;
 }
 
@@ -58,6 +64,7 @@ Node* create_binary_op(char op, Node* left, Node* right) {
     node->data.binary.op = op;
     node->left = left;
     node->right = right;
+    node->extra = NULL;
     return node;
 }
 
@@ -66,6 +73,7 @@ Node* create_unary_op(char op, Node* right) {
     node->type = NODE_UNARY_OP;
     node->data.binary.op = op;
     node->right = right;
+    node->left = node->extra = NULL;
     return node;
 }
 
@@ -73,6 +81,7 @@ Node* create_grouping(Node* expr) {
     Node* node = (Node*)allocate(sizeof(Node));
     node->type = NODE_GROUPING;
     node->left = expr;
+    node->right = node->extra = NULL;
     return node;
 }
 
@@ -80,6 +89,7 @@ Node* create_expression_stmt(Node* expr) {
     Node* node = (Node*)allocate(sizeof(Node));
     node->type = NODE_EXPRESSION_STMT;
     node->left = expr;
+    node->right = node->extra = NULL;
     return node;
 }
 
@@ -88,6 +98,7 @@ Node* create_var_decl(const char* name, Node* initializer) {
     node->type = NODE_VARIABLE_DECL;
     node->data.identifier = copy_string(name);
     node->left = initializer;
+    node->right = node->extra = NULL;
     return node;
 }
 
@@ -96,6 +107,7 @@ Node* create_assignment(const char* name, Node* value) {
     node->type = NODE_ASSIGNMENT;
     node->data.identifier = copy_string(name);
     node->left = value;
+    node->right = node->extra = NULL;
     return node;
 }
 
@@ -103,6 +115,7 @@ Node* create_print(Node* expr) {
     Node* node = (Node*)allocate(sizeof(Node));
     node->type = NODE_PRINT_STMT;
     node->left = expr;
+    node->right = node->extra = NULL;
     return node;
 }
 
@@ -110,6 +123,7 @@ Node* create_getlf(Node* expr) {
     Node* node = (Node*)allocate(sizeof(Node));
     node->type = NODE_GETLF_STMT;
     node->left = expr;
+    node->right = node->extra = NULL;
     return node;
 }
 
@@ -117,6 +131,7 @@ Node* create_input(const char* prompt) {
     Node* node = (Node*)allocate(sizeof(Node));
     node->type = NODE_INPUT_STMT;
     node->data.string_value = copy_string(prompt ? prompt : "");
+    node->left = node->right = node->extra = NULL;
     return node;
 }
 
@@ -134,6 +149,7 @@ Node* create_while(Node* condition, Node* body) {
     node->type = NODE_WHILE_STMT;
     node->left = condition;
     node->right = body;
+    node->extra = NULL;
     return node;
 }
 
@@ -142,25 +158,25 @@ Node* create_for(Node* initializer, Node* condition, Node* increment, Node* body
     node->type = NODE_FOR_STMT;
     node->left = initializer;
     node->right = condition;
-    node->extra = increment;
-    // Le body est dans l'extra du noeud condition? Non, on va utiliser une structure spéciale
-    // Pour simplifier, on va faire un bloc spécial
-    Node* for_node = (Node*)allocate(sizeof(Node));
-    for_node->type = NODE_FOR_STMT;
-    for_node->left = initializer;
-    for_node->right = condition;
-    for_node->extra = increment;
-    // On attache le body à l'extra de l'extra
-    // C'est un peu moche, on pourrait améliorer avec une structure dédiée
-    return for_node;
+    
+    // Créer un noeud spécial pour contenir l'incrément et le corps
+    Node* for_body = (Node*)allocate(sizeof(Node));
+    for_body->type = NODE_BLOCK; // On réutilise le type BLOCK
+    for_body->left = body;
+    for_body->right = increment;
+    for_body->extra = NULL;
+    
+    node->extra = for_body;
+    return node;
 }
 
 Node* create_function(const char* name, Node* params, Node* body) {
     Node* node = (Node*)allocate(sizeof(Node));
     node->type = NODE_FUNCTION_DECL;
     node->data.identifier = copy_string(name);
-    node->left = params;  // Liste des paramètres
-    node->right = body;   // Corps de la fonction
+    node->left = params;
+    node->right = body;
+    node->extra = NULL;
     return node;
 }
 
@@ -168,7 +184,8 @@ Node* create_function_call(const char* name, Node* args) {
     Node* node = (Node*)allocate(sizeof(Node));
     node->type = NODE_FUNCTION_CALL;
     node->data.identifier = copy_string(name);
-    node->left = args;    // Liste des arguments
+    node->left = args;
+    node->right = node->extra = NULL;
     return node;
 }
 
@@ -176,6 +193,7 @@ Node* create_return(Node* value) {
     Node* node = (Node*)allocate(sizeof(Node));
     node->type = NODE_RETURN_STMT;
     node->left = value;
+    node->right = node->extra = NULL;
     return node;
 }
 

89e1d474282b820d833426d5a760d8e15d0bfb2e
Mauricio-100 <ceoseshell@gmail.com>
2026-03-18 23:17:04 +0100

    Update module.c

--- src/module.c
+++ src/module.c
@@ -12,31 +12,29 @@ static fe_Object* load_module(fe_Context *ctx, fe_Object *args, int is_local) {
   FILE *fp;
   int gc;
   fe_Object *obj;
+  fe_Object *result = fe_bool(ctx, 1); /* 't' par défaut */
 
   target_symbol[0] = '\0';
   fe_tostring(ctx, arg, path_buf, sizeof(path_buf));
 
-  /* 1. Extraction du chemin et du symbole cible */
+  /* 1. Extraction du chemin et du symbole :: */
   first_sep = strstr(path_buf, "::");
   if (first_sep) {
-    /* On cherche le dernier segment après le dernier ':' */
     char *last_colon = strrchr(path_buf, ':');
     if (last_colon) {
       strcpy(target_symbol, last_colon + 1);
     }
-    /* On coupe la chaîne au premier ':' pour avoir le nom du fichier */
     *first_sep = '\0';
   }
 
-  /* 2. Construction du chemin du fichier */
+  /* 2. Construction du chemin */
   if (is_local) {
     sprintf(file_path, "./%s.fe", path_buf);
   } else {
-    /* Chemin global par défaut */
     sprintf(file_path, "/usr/local/lib/goscript/%s.fe", path_buf);
   }
 
-  /* 3. Ouverture et lecture du fichier */
+  /* 3. Ouverture */
   fp = fopen(file_path, "rb");
   if (!fp) {
     char err_msg[512];
@@ -44,7 +42,7 @@ static fe_Object* load_module(fe_Context *ctx, fe_Object *args, int is_local) {
     fe_error(ctx, err_msg);
   }
 
-  /* 4. Évaluation du contenu du fichier */
+  /* 4. Évaluation sécurisée */
   gc = fe_savegc(ctx);
   while ((obj = fe_readfp(ctx, fp))) {
     fe_eval(ctx, obj);
@@ -52,16 +50,15 @@ static fe_Object* load_module(fe_Context *ctx, fe_Object *args, int is_local) {
   }
   fclose(fp);
 
-  /* 5. Retourne l'objet spécifique si demandé, sinon retourne 't' */
+  /* 5. Retour de l'objet ou de 't' */
   if (target_symbol[0] != '\0') {
-    fe_Object *sym = fe_symbol(ctx, target_symbol);
-    return fe_eval(ctx, sym);
+    /* On cherche le symbole dans l'environnement global */
+    result = fe_eval(ctx, fe_symbol(ctx, target_symbol));
   }
 
-  return fe_bool(ctx, 1);
+  return result;
 }
 
-/* Fonctions exportées */
 fe_Object* f_use(fe_Context *ctx, fe_Object *args) {
   return load_module(ctx, args, 0);
 }

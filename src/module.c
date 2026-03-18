46a6d356d86db7e8deb88d8cde59d97befbc2384
Mauricio-100 <ceoseshell@gmail.com>
2026-03-18 23:10:42 +0100

    Update module.c

--- src/module.c
+++ src/module.c
@@ -3,43 +3,48 @@
 #include <string.h>
 #include "fe.h"
 
-/* Cette fonction reste statique car elle n'est utilisée que dans ce fichier */
 static fe_Object* load_module(fe_Context *ctx, fe_Object *args, int is_local) {
   fe_Object *arg = fe_nextarg(ctx, &args);
   char path_buf[256];
   char file_path[512];
-  char target_symbol[128] = "";
+  char target_symbol[128];
   char *first_sep;
   FILE *fp;
   int gc;
   fe_Object *obj;
 
+  target_symbol[0] = '\0';
   fe_tostring(ctx, arg, path_buf, sizeof(path_buf));
 
+  /* 1. Extraction du chemin et du symbole cible */
   first_sep = strstr(path_buf, "::");
   if (first_sep) {
-    char *last_sep, *temp;
-    *first_sep = '\0'; 
-    last_sep = first_sep + 2;
-    while ((temp = strstr(last_sep, "::")) != NULL) {
-      last_sep = temp + 2;
+    /* On cherche le dernier segment après le dernier ':' */
+    char *last_colon = strrchr(path_buf, ':');
+    if (last_colon) {
+      strcpy(target_symbol, last_colon + 1);
     }
-    strcpy(target_symbol, last_sep);
+    /* On coupe la chaîne au premier ':' pour avoir le nom du fichier */
+    *first_sep = '\0';
   }
 
+  /* 2. Construction du chemin du fichier */
   if (is_local) {
-    snprintf(file_path, sizeof(file_path), "./%s.fe", path_buf);
+    sprintf(file_path, "./%s.fe", path_buf);
   } else {
-    snprintf(file_path, sizeof(file_path), "/usr/local/lib/goscript/%s.fe", path_buf);
+    /* Chemin global par défaut */
+    sprintf(file_path, "/usr/local/lib/goscript/%s.fe", path_buf);
   }
 
+  /* 3. Ouverture et lecture du fichier */
   fp = fopen(file_path, "rb");
   if (!fp) {
-    char err[512];
-    snprintf(err, sizeof(err), "Module not found: %s", file_path);
-    fe_error(ctx, err);
+    char err_msg[512];
+    sprintf(err_msg, "Module not found: %s", file_path);
+    fe_error(ctx, err_msg);
   }
 
+  /* 4. Évaluation du contenu du fichier */
   gc = fe_savegc(ctx);
   while ((obj = fe_readfp(ctx, fp))) {
     fe_eval(ctx, obj);
@@ -47,13 +52,16 @@ static fe_Object* load_module(fe_Context *ctx, fe_Object *args, int is_local) {
   }
   fclose(fp);
 
+  /* 5. Retourne l'objet spécifique si demandé, sinon retourne 't' */
   if (target_symbol[0] != '\0') {
-    return fe_eval(ctx, fe_symbol(ctx, target_symbol));
+    fe_Object *sym = fe_symbol(ctx, target_symbol);
+    return fe_eval(ctx, sym);
   }
+
   return fe_bool(ctx, 1);
 }
 
-/* Ces fonctions NE DOIVENT PAS être static pour être vues par fe.c */
+/* Fonctions exportées */
 fe_Object* f_use(fe_Context *ctx, fe_Object *args) {
   return load_module(ctx, args, 0);
 }

5c99401a9821f77793678861048a20130a40536f
Mauricio-100 <ceoseshell@gmail.com>
2026-03-18 22:50:04 +0100

    Update module.c

--- src/module.c
+++ src/module.c
@@ -4,70 +4,56 @@
 #include "fe.h"
 
 static fe_Object* load_module(fe_Context *ctx, fe_Object *args, int is_local) {
-  /* Récupérer le premier argument (symbole ou string) */
   fe_Object *arg = fe_nextarg(ctx, &args);
   char path_buf[256];
-  fe_tostring(ctx, arg, path_buf, sizeof(path_buf));
-
   char file_path[512];
   char target_symbol[128] = "";
+  char *first_sep;
+  FILE *fp;
+  int gc;
+  fe_Object *obj;
+
+  fe_tostring(ctx, arg, path_buf, sizeof(path_buf));
 
   /* Parser le format : dossier/fichier::type::nom */
-  char *first_sep = strstr(path_buf, "::");
+  first_sep = strstr(path_buf, "::");
   if (first_sep) {
-    *first_sep = '\0'; /* path_buf contient maintenant uniquement le chemin */
+    char *rest, *last_sep, *temp;
+    *first_sep = '\0'; 
     
-    /* Trouver la dernière partie pour extraire le nom de la fonction/objet */
-    char *rest = first_sep + 2;
-    char *last_sep = rest;
-    char *temp;
+    rest = first_sep + 2;
+    last_sep = rest;
     while ((temp = strstr(last_sep, "::")) != NULL) {
       last_sep = temp + 2;
     }
     strcpy(target_symbol, last_sep);
-  } else {
-    /* Pas de '::', on charge juste le fichier */
-    strcpy(file_path, path_buf);
   }
 
-  /* Construire le chemin absolu ou relatif avec l'extension .fe */
+  /* Construire le chemin */
   if (is_local) {
     snprintf(file_path, sizeof(file_path), "./%s.fe", path_buf);
   } else {
-    /* TODO: Tu peux changer ce chemin pour pointer vers ta bibliothèque standard */
-    snprintf(file_path, sizeof(file_path), "/usr/local/lib/ton_langage/%s.fe", path_buf);
+    snprintf(file_path, sizeof(file_path), "/usr/local/lib/goscript/%s.fe", path_buf);
   }
 
-  /* Ouvrir et évaluer le fichier */
-  FILE *fp = fopen(file_path, "rb");
+  /* Ouvrir et évaluer */
+  fp = fopen(file_path, "rb");
   if (!fp) {
     char err[512];
     snprintf(err, sizeof(err), "Module not found: %s", file_path);
     fe_error(ctx, err);
   }
 
-  int gc = fe_savegc(ctx);
-  fe_Object *obj;
+  gc = fe_savegc(ctx);
   while ((obj = fe_readfp(ctx, fp))) {
     fe_eval(ctx, obj);
     fe_restoregc(ctx, gc);
   }
   fclose(fp);
 
-  /* Si on a demandé un objet spécifique (ex: 'str'), on le cherche et on le retourne */
   if (target_symbol[0] != '\0') {
-    fe_Object *sym = fe_symbol(ctx, target_symbol);
-    return fe_eval(ctx, sym); /* Retourne la valeur liée à ce symbole */
+    return fe_eval(ctx, fe_symbol(ctx, target_symbol));
   }
 
-  return fe_bool(ctx, 1); /* Retourne 't' (vrai) si tout s'est bien passé mais sans cible précise */
-}
-
-/* Enveloppes pour les fonctions natives Lisp */
-fe_Object* f_use(fe_Context *ctx, fe_Object *args) {
-  return load_module(ctx, args, 0); /* Global */
-}
-
-fe_Object* f_nm(fe_Context *ctx, fe_Object *args) {
-  return load_module(ctx, args, 1); /* Local */
+  return fe_bool(ctx, 1);
 }

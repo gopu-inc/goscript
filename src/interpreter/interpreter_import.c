f57a36da4b3ad525626bebacb3649d1734e874ee
Mauricio-100 <ceoseshell@gmail.com>
2026-04-24 15:21:06 +0100

    Add additional search paths for module imports’

--- src/interpreter/interpreter_import.c
+++ src/interpreter/interpreter_import.c
@@ -237,6 +237,10 @@ char* resolve_module_path(char* current_file, char* import_path) {
     const char* search_paths[] = {
         "./modules",
         "./lib",
+        "../lib",
+        "../modules",
+        "../../lib",
+        "../../modules",
         "./lib/std",
         "./src",
         "/usr/local/lib/goscript",
@@ -556,10 +560,16 @@ static LoadedModule* load_file_module(const char* full_path, const char* module_
                 case NODE_EXPORT:
                     register_export(mod, stmt->export.name, NULL);
                     break;
+                
+                case NODE_IMPORT:
+                    evaluate_statement(stmt, mod->env, full_path);
+                    break;
+                // ----------------------------------------------
                     
                 default:
                     break;
             }
+
         }
     }
     
